840ca4e60325ab3068c360ea005979d2b75f4b1c
bas-boop <dereusbas461@gmail.com>
2024-07-18 23:04:07 +0200

    Added train system

---
+++ Assets/Scripts/Framework/RandomMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Framework
+{
+    /// <summary>
+    /// A test script so something moves randomly in all 3 dimensions.
+    /// </summary>
+    public sealed class RandomMovement : MonoBehaviour
+    {
+        [SerializeField] private float speed = 5f;
+        
+        private readonly Vector3 _cubeCenter = Vector3.zero;
+        private readonly Vector3 _cubeSize = new (25, 25, 25);
+        private Vector3 _targetPosition;
+
+        private void Start() => SetNewTargetPosition();
+
+        private void Update() => MoveTowardsTarget();
+
+        private void SetNewTargetPosition()
+        {
+            float x = Random.Range(_cubeCenter.x - _cubeSize.x / 2, _cubeCenter.x + _cubeSize.x / 2);
+            float y = Random.Range(_cubeCenter.y - _cubeSize.y / 2, _cubeCenter.y + _cubeSize.y / 2);
+            float z = Random.Range(_cubeCenter.z - _cubeSize.z / 2, _cubeCenter.z + _cubeSize.z / 2);
+
+            _targetPosition = new Vector3(x, y, z);
+        }
+
+        private void MoveTowardsTarget()
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, _targetPosition, step);
+            
+            if (Vector3.Distance(transform.position, _targetPosition) < 0.001f)
+                SetNewTargetPosition();
+        }
+    }
+}
---
+++ Assets/Scripts/Framework/TrainMovement/TrainStats.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Framework.TrainMovement
+{
+    [Serializable]
+    public struct TrainStats
+    {
+        [Range(0.1f, 25)] public float followDistance;
+        [Range(0.1f, 1)] public float smoothSpeed;
+
+        private TrainStats(float? a, float? b)
+        {
+            followDistance = a ?? 0.5f;
+            smoothSpeed = b ?? 1f;
+        }
+    }
+}
