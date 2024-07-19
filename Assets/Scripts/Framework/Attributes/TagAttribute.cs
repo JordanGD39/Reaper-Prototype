6913d46ce1ab6c89c02011f556c9548f01ed5718
bas-boop <dereusbas461@gmail.com>
2024-07-19 15:55:34 +0200

    Added tag attribute

---
+++ Assets/Scripts/Framework/Attributes/TagAttribute.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Framework.Attributes
+{
+    /// <summary>
+    /// Should only be used on a string or a collection of strings.
+    /// </summary>
+    public sealed class TagAttribute : PropertyAttribute { }
+}
